5890baa8d7c116e8a0e1bae6cf61b5b85911aa34
a3523110714@alumno.uttehuacan.edu.mx <a3523110714@alumno.uttehuacan.edu.mx>
2025-01-19 14:53:48 -0600

    inicioApp

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import React, { useState } from 'react';
+import { MeiliSearch } from 'meilisearch';
+
+const client = new MeiliSearch({
+  host: 'http://18.227.13.140',
+  apiKey: 'c716781c75b1115ae1bd945fd73b87d2f12a5f2e878cfc6fbe45f68d57be',
+});
