1e12e9a046dfa01cb13bbd684904db6fb7884cac
a3523110714@alumno.uttehuacan.edu.mx <a3523110714@alumno.uttehuacan.edu.mx>
2025-01-19 15:36:41 -0600

    1 estrutura

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
-import React, { useState } from 'react';
-import { MeiliSearch } from 'meilisearch';
 
-const client = new MeiliSearch({
-  host: 'http://18.227.13.140',
-  apiKey: 'c716781c75b1115ae1bd945fd73b87d2f12a5f2e878cfc6fbe45f68d57be',
-});
+// src/App.js
+import React, { useState } from 'react';
+import './App.css';
+
+const App = () => {
+  const [query, setQuery] = useState('');
+  return (
+    <div className="container">
+      <h1 className="title">¿Qué película deseas ver?</h1>
+      <input
+        type="text"
+        placeholder="Escribe la pelicula que desees"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        className="input"
+      />
+    </div>
+  );
+};
+
+export default App;
--- src/searchClient.js
+++ src/searchClient.js
@@ -1,9 +1,1 @@
 
-import { instantMeiliSearch } from '@meilisearch/instant-meilisearch';
-
-const { searchClient } = instantMeiliSearch(
-  'http://18.227.13.140',
-  'c716781c75b1115ae1bd945fd73b87d2f12a5f2e878cfc6fbe45f68d57be'
-);
-
-export { searchClient };
